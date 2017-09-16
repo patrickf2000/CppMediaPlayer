495a9bad8b7a98c55f0cfefb2f20915e928c3552
pdf4664 <pdf4664@outlook.com>
2017-09-16 18:22:01 -0400

    added no-theme-icon support for video pane context menu

--- src/videopane_menu.cxx
+++ src/videopane_menu.cxx
@@ -1,13 +1,31 @@
 //videopane_menu.cxx
+#include <QPixmap>
+
 #include "videopane_menu.hh"
 #include "actions.hh"
 #include "videopane.hh"
 
 VideoPaneMenu::VideoPaneMenu() {
-	open = new QAction(QIcon::fromTheme("document-open"),"Open",this);
-	play = new QAction(QIcon::fromTheme("media-playback-start"),"Play",this);
-	pause = new QAction(QIcon::fromTheme("media-playback-pause"),"Pause",this);
-	stop = new QAction(QIcon::fromTheme("media-playback-stop"),"Stop",this);
+    open = new QAction("Open",this);
+    play = new QAction("Play",this);
+    pause = new QAction("Pause",this);
+    stop = new QAction("Stop",this);
+
+    QPixmap documentOpenIcon(":/icons/document-open.png");
+    QPixmap playIcon(":/icons/media-playback-start.png");
+    QPixmap pauseIcon(":/icons/media-playback-pause.png");
+    QPixmap stopIcon(":/icons/media-playback-stop.png");
+#ifdef NO_THEME_ICONS
+    open->setIcon(documentOpenIcon);
+    play->setIcon(playIcon);
+    pause->setIcon(pauseIcon);
+    stop->setIcon(stopIcon);
+#else
+    open->setIcon(QIcon::fromTheme("document-open",documentOpenIcon));
+    play->setIcon(QIcon::fromTheme("media-playback-start",playIcon));
+    pause->setIcon(QIcon::fromTheme("media-playback-pause",pauseIcon));
+    stop->setIcon(QIcon::fromTheme("media-playback-stop",stopIcon));
+#endif
 	
 	connect(open,&QAction::triggered,this,&VideoPaneMenu::onOpenClicked);
 	connect(play,&QAction::triggered,this,&VideoPaneMenu::onPlayClicked);
